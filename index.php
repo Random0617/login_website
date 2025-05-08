c4676c391d7b10d06b75be0985dc8bee4dfea6c9
Tran Thuc Minh Tri <minhtri170603@gmail.com>
2025-05-08 09:43:04 +0700

    Moodle custom OAuth partially finished, db not modified yet

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 </head>
 <body>
     <h3>Welcome to Demo website</h3>
-    <h3>You are not currently logged in.</h3>
+    <h3 id="login_state">You are not currently logged in.</h3>
     <button onclick="location.href='login.php'">
         Log in
     </button>
@@ -14,3 +14,70 @@
     </button>
 </body>
 </html>
+<?php
+$email = "";
+if (isset($_GET['code'])) {
+    $code = $_GET['code'];
+    echo $code . "\n";
+
+    // Step 3: https://github.com/Random0617/moodle-local_oauth-modified?tab=readme-ov-file#how-to-use
+    $token_url = "http://localhost/MyMoodleSite/local/oauth/token.php";
+
+    $data = [
+        'code' => $code,
+        'client_id' => 'login_website',
+        'client_secret' => '84f1065ba7b3828cd1eab494b246ce817ea6ff0a099660e4',
+        'grant_type' => 'authorization_code',
+        'scope' => 'user_info'
+    ];
+
+    $ch = curl_init($token_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+    $response = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    } else {
+        $responseData = json_decode($response, true);
+        echo '<pre>';
+        echo json_encode($responseData, JSON_PRETTY_PRINT);
+        echo '</pre>';
+    }
+    curl_close($ch);
+
+    // Step 6: https://github.com/Random0617/moodle-local_oauth-modified?tab=readme-ov-file#how-to-use
+    $token_url = "http://localhost/MyMoodleSite/local/oauth/user_info.php";
+
+    $data = [
+        'access_token' => $responseData["access_token"]
+    ];
+
+    $ch = curl_init($token_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+
+    $response = curl_exec($ch);
+
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    } else {
+        $responseData = json_decode($response, true);
+        echo '<pre>';
+        echo json_encode($responseData, JSON_PRETTY_PRINT);
+        echo '</pre>';
+    }
+    curl_close($ch);
+    
+    $email = $responseData["email"];
+    
+    echo "Email: " . $email;
+}
+?>
+<script>
+    const email_string = <?php echo json_encode($email); ?>;
+    document.getElementById("login_state") = "Logged in as ".concat(email_string);
+</script>
--- login.php
+++ login.php
@@ -15,44 +15,9 @@
         
         <button type="submit">Login normally</button>
     </form>
-    <button onclick="loginMoodle()">Login using Moodle</button>
+    <button onclick="window.location.href = 'http://localhost/MyMoodleSite/local/oauth/login.php?client_id=login_website&response_type=code';">Login using Moodle</button>
     <button onclick="location.href='index.php'">
         Back to home page
     </button>
 </body>
 </html>
-<script>
-    
-    async function loginMoodle() {
-        window.location.href = 'http://localhost/MyMoodleSite/local/oauth/login.php?client_id=login_website&response_type=code';
-        /*
-        const urlParams = new URLSearchParams(window.location.search);
-        const code = urlParams.get('code');
-        
-        try {
-            const url = 'http://moodledomain.com/local/oauth/token.php';
-            
-            const params = new URLSearchParams({
-                code: code,
-                client_id: 'login_website',
-                client_secret: '84f1065ba7b3828cd1eab494b246ce817ea6ff0a099660e4',
-                grant_type: 'authorization_code',
-                scope: 'user_info'
-            });
-
-            const response = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: params
-            });
-
-            const data = await response.json();
-            alert('OAuth Token Response: ', data);
-        } catch (error) {
-            alert('Error:', error);
-        } 
-        */
-    }
-</script>
